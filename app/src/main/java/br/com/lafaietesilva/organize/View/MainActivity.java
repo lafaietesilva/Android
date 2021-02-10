f71de7612ffef2ff907b5df54a2e3cefcf703c3f
lafaietesilva <jorgelafaiete.silva@gmail.com>
2021-02-10 15:29:39 -0300

    control

--- app/src/main/java/br/com/lafaietesilva/organize/View/MainActivity.java
+++ app/src/main/java/br/com/lafaietesilva/organize/View/MainActivity.java
@@ -5,17 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import br.com.lafaietesilva.organize.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "ORGANIZE";
+    FloatingActionButton btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG,"Abrindo o activy");
+        Log.d(TAG,"Abrindo o Activity");
+
+        btnAdd = findViewById(R.id.btnAdd);
     }
 }
